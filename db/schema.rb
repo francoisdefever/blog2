e333dcb5ff0760076af6867ecafe60674c9bd797
francoisdefever <fdefever.work@gmail.com>
2016-02-11 11:43:47 +0100

    Create Post DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210155757) do
+ActiveRecord::Schema.define(version: 20160211104302) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "user"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
