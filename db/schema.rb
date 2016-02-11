2716d67f052d54fa959780a82924c9405fb38705
francoisdefever <fdefever.work@gmail.com>
2016-02-11 09:54:06 +0100

    Username connexion | add avatar to sign up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210132651) do
+ActiveRecord::Schema.define(version: 20160210155757) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
